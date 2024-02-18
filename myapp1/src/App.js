15e0604d988ccb08b8b221d38cc30c97cca295bd
rahulmane20122000 <rahulmane20122000@gmail.com>
2024-02-18 18:55:42 +0530

    useEffect implemented

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrease = () => {
-    count > 0 ? setCount(count - 1) : setCount(0);
-  };
+  useEffect(()=>{
+    alert('you changed counter to : ' + count);
+  },[count])
   return (
     <div className="App">
       <button onClick={()=> setCount((prevCount)=> prevCount + 1)}>+</button>
